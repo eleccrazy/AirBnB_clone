98f97af6139e62d734195b53afec21827cea09e4
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-04 14:46:32 -0400

    console test

---
+++ tests/test_console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+File: test_console.py
+Desc: This module contains all possible testcases for the console.py
+      modlue. It uses the standard unittest.
+Author: Gizachew Bayness (Elec Crazy) and Biruk Gelelcha
+Date Created: Sep 4 2022
+"""
+import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+import sys
+from io import StringIO
+
+
+class TestConsolePromport(unittest.TestCase):
+    """
+    This class provides all possible test cases regarding prompt
+    response of class Amenity.
+    """
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
